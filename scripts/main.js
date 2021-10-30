aa30b28d5b0e89825e7e0a71b1087e9e6ad7df4b
arthurbright <55501753+arthurbright@users.noreply.github.com>
2021-10-30 04:49:47 -0400

    add projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 const headerTitle = document.getElementById("headerTitle");
 const backgroundPic = document.getElementById("backgroundPic");
 const title1 = document.getElementById("title1");
+const namee = document.getElementById("namee")
 const icon = document.getElementById("logo")
 const arrow = document.getElementById("arrow");
 
@@ -21,17 +22,29 @@ arrow.addEventListener('click', (e)=>{
     window.location.href = "#aboutme"
 })
 
-window.onmousemove = (e)=>{
-    //parallax background
-    const parallaxRatio = 0.02;
+
+
+
+const lerp = 0.15;
+const lerpp = 1 - lerp;
+const offset = 30;
+curtop = titleY;
+curleft = window.innerWidth/2;
+curtop2 = 0;
+curleft2 = 0;
+const parallaxRatio = 0.02;
+
+const parallax = (e)=>{
     var height = window.innerHeight;
     var width = window.innerWidth;
-    const offset = 30;
+
     
     var x = -parallaxRatio * (e.clientX - width/2);
     var y = -parallaxRatio * (e.clientY - height/2);
-    backgroundPic.style.left = (x - offset) + "px";
-    backgroundPic.style.top = (y - offset) + "px";
+    backgroundPic.style.left = (curleft2 * lerpp  + (x - offset) * lerp) + "px";
+    backgroundPic.style.top = (curtop2 * lerpp + (y - offset) * lerp)+ "px";
+    curtop2 = (curtop2 * lerpp + (y - offset) * lerp);
+    curleft2 = (curleft2 * lerpp  + (x - offset) * lerp);
 
 
     //moving the text
@@ -40,6 +53,34 @@ window.onmousemove = (e)=>{
     var x2 = -parallaxRatio2 * (e.clientX - width/2);
     var y2 = -parallaxRatio2 * (e.clientY - height/2);
     //offset position so still near center
-    title1.style.left = (x2 + width/2) + "px";
-    title1.style.top = (y2 + titleY) + "px";
+    title1.style.left = (curleft * lerpp + (x2 + width/2) * lerp) + "px";
+    title1.style.top = (curtop * lerpp + (y2 + titleY) * lerp) + "px";
+    curleft = (curleft * lerpp + (x2 + width/2) * lerp);
+    curtop =  (curtop * lerpp + (y2 + titleY) * lerp);
+}
+
+window.onmousemove = (e) =>{
+    //parallax background
+    parallax(e);
+
+    
+ }
+
+ const NAME = "Arthur Bright"
+ letters = -1;
+ window.onload = (e)=>{
+    window.setInterval(() =>{
+        if(letters < 13){
+            letters ++;
+            if(letters == 13){
+                namee.innerHTML = NAME.substring(0, letters);
+            }
+            else{
+                namee.innerHTML = NAME.substring(0, letters) + "_";
+            }
+            
+            console.log(letters);
+        }
+    }, 80);
+
  }
