957285d7512b2f8070560fda50d561c3cab602f1
arthurbright <artb1234567@gmail.com>
2025-06-03 00:12:56 -0700

    gg

--- guoguessr/main.js
+++ guoguessr/main.js
@@ -6,25 +6,36 @@ const liveCounter = document.getElementById('lives');
 var q = -1;
 
 var qs = [
-  ["_ _ _ _ _ &nbsp;&nbsp; _ _ _ _ _ _", "civic.jpg", "civiccentre", 8, -105, 20],
-      ["_ _ _ _", "deca.png", "deca", 20, 0, 100],
-["In what building was this image taken?", "bagman_tnt.jpg", "t&t", 25, -7, 15],
-  ["_ _ _ _ _ _ _ &nbsp;&nbsp;&nbsp; _ _ _ _ _ _!", "mc.jpg", "chickenjockey", 17, -60, 40],
-    ["Building?", "cmh.png", "cmh", 5, 0, -200],
-      ["Song?", "baboons.jpg", "baboons", 10, 40, -50],
-        ["City?", "tobermory.jpg", "tobermory", 14, -140, 20],
-        ["?", "decimate.png", "apprehend", 3, 0, 0],
-      ["Room number?", "505.jpg", "505", 5, 25, -20],
-        ["_ _ _ _ _", "cotty.jpg", "cotty", 17, -85, 0],
-          ["PFP where?", "linkedin.jpg", "linkedin", 4, 0, 10],
-            ["Who was in the room with you? alphabetical first names eg. 'Alice Bob'", "who.jpg", "lavan sat", 10, 25, 25],
-  ["City?", "kitchener.jpg", "kitchener", 6, 0, 70],
+//       ["_ _ _ _", "deca.png", "icdc", 20, 0, 100],
+// ["In what building was this image taken?", "bagman_tnt.jpg", "t&t", 25, -7, 15],
+//   ["_ _ _ _ _ _ _ &nbsp;&nbsp;&nbsp; _ _ _ _ _ _!", "mc.jpg", "chickenjockey", 17, -60, 40],
+//     ["Building?", "cmh.png", "cmh", 5, 0, -200],
+//       ["Song?", "baboons.jpg", "baboons", 10, 40, -50],
+//         ["_ _ _ _ _ _ _ 's", "bluffer.jpg", "bluffer", 8, -60, -20],
+//         ["_ _ _", "ion.jpg", "ion", 8, -100, -20],
+//             ["_ _ _ _", "deca2.png", "deca", 6, 0, 40],
+//           ["House?", "lavan.jpg", "lavan", 20, -130, 60],
+//         ["City?", "tobermory.jpg", "tobermory", 14, -140, 20],
+//           ["_ _ _ _ _ &nbsp;&nbsp; _ _ _ _ _ _", "civic.jpg", "civiccentre", 8, -105, 20],
+//   ["⛰️?", "ski.jpg", "tremblant", 10, -130, 90],
+//         ["?", "decimate.png", "apprehend", 3, 0, 0],
+//       ["Room number?", "505.jpg", "505", 5, 25, -20],
+//       ["Store?", "uniqlo.jpg", "uniqlo", 10, 0, -30],
+//         ["_ _ _ _ _", "cotty.jpg", "cotty", 17, -85, 0],
+//           ["Course?", "music270.png", "music270", 8, -25, 60],
+//           ["PFP where?", "linkedin.jpg", "linkedin", 4, 0, 10],
+//             ["Who was in the room with you? alphabetical first names eg. 'Alice Bob'", "who.jpg", "lavansat", 10, 25, 25],
+//   ["City?", "kitchener.jpg", "kitchener", 6, 0, 70],
+  ["Excellent work. But I don't think we're done here. Are you ready for a challenge?", "hacker.png", "yes" , 1, -70, 0],
+  ["Take all of the answers you have submitted so far. Remove non-alphabetic characters, and convert to lower case. Find a shortest string that contains all of these strings as substrings, along with the additional strings ['kevin', 'guo', 'eye', 'hamlet', 'ferb', 'balloon', 'etch', 'made', 'lock', 'minecraft', 'ayo', 'obama', 'abc', 'hola', 'slink', 'onset', 'sap', 'gum', 'yunshang', 'oink', 'glow', 'minor', 'antidisestablishmentarianism'].", "hacker.png", "AWOOGA" , 1, -70, 0]
 ]
 
 var scale = -1;
 var x = -1;
 var y = -1;
 var lives = 100;
+
+
 
 
 function cleanString(str) {
@@ -35,7 +46,7 @@ function zoom_out() {
   lives -= 1;
   liveCounter.innerHTML = "Lives Remaining: " + lives.toString();
 
-  factor = 0.93;
+  factor = 0.86;
   scale = Math.max(1, scale * factor);
   x = x * factor;
   y = y * factor;
@@ -49,7 +60,13 @@ function zoom_out() {
 
 
 function next_q() {
+  
   q += 1;
+  if(q == qs.length){
+    // TODO redirect
+    window.location.replace("finish.html");
+  }
+
   if(q == 1){
     title.remove();
   }
@@ -76,6 +93,18 @@ function submitAnswer() {
       const correctAnswer = qs[q][2]; // You can change this
 
       const feedback = document.getElementById('feedback');
+
+      if(q == qs.length - 1){
+        if(check(userAnswer)){
+          next_q();
+        }
+        else{
+          feedback.textContent = "Try again.";
+          feedback.style.color = "red";
+        }
+        return;
+      }
+      
       if (userAnswer.includes(correctAnswer)) {
         feedback.textContent = "Correct!";
         feedback.style.color = "green";
@@ -88,26 +117,21 @@ function submitAnswer() {
     }
 
 
+// ans
+arr = ['icdc', 'tt', 'chickenjockey', 'cmh', 'baboons', 'bluffer', 'ion', 'deca', 'lavan', 'tobermory', 'civiccentre', 'tremblant', 'apprehend', 'uniqlo', 'cotty', 'music', 'linkedin', 'lavansat', 'kitchener', 'yes', 'kevin', 'guo', 'eye', 'hamlet', 'ferb', 'balloon', 'etch', 'made', 'lock', 'minecraft', 'ayo', 'obama', 'abc', 'hola', 'slink', 'onset', 'sap', 'gum', 'yunshang', 'oink', 'glow', 'minor', 'antidisestablishmentarianism'];
 
-// ideas:
-/*
-Deca balloon picture
-Guess the song (from a picture of sheet music)
-Guess the song (from a melody eg 3 2 1 2 3 3 3)
-Album cover (niki?)
-Cotty
-Ski trip
-High school pics???
-Band!
-look thru google photos albums
--- pinned images in discord
+function check(s){
+    if(s.length != 202){
+      return false;
+    }
+    for(i in arr){
+      ss = arr[i];
+      if(!s.includes(ss)){
+        // console.log(i);
+        return false;
+      }
+    }
+    return true;
+  }
 
--- for photos of kevin in them, start zoomed in to his face
--- then, slowly zoom out per wrong answer
-
--- answer should not be ambiguous, eg "when was this" could have multiple answers
-
-
-
-
-*/
+// guobamadecabciviccentremblantidisestablishmentarianismusicdcmholavansattobermoryunshanglowsapprehendhamletballoonuniqlockitchenerioncottyminorguminecraftblufferbaboonsetchickenjockeyeslinkedinayoinkevin
