031b755ed7e05ecf112d532d0f1a5e29312da043
arthurbright <artb1234567@gmail.com>
2026-09-15 23:56:11 -0400

    start refactor

---
+++ src/dir_gen.py
@@ -0,0 +1,51 @@
+# IMPORTANT: THIS MUST BE RUN FROM THE ROOT DIRECTORY (not within src)
+
+import json
+
+from pathlib import Path
+
+def get_directory_structure(directory):
+    directory = Path(directory)
+
+    structure = {
+        "name": directory.name,
+        "type": "directory",
+        "children": []
+    }
+
+    for item in sorted(directory.iterdir()):
+        # directory
+        if item.is_dir():
+            structure["children"].append(
+                get_directory_structure(item)
+            )
+        elif item.is_file():
+            if(item.name.endswith(".txt")):
+                # text file; read contents
+                with open(item.absolute(), "r", encoding="utf-8") as f:
+                    contents = f.read()
+
+                structure["children"].append({
+                    "name": item.name[:-4],
+                    "type": "file",
+                    "contents": contents
+                })
+            else:
+                # other file; serve it directly
+                structure["children"].append({
+                    "name": item.name,
+                    "type": "served_file",
+                    "full_path": str(item.relative_to(Path("./")))
+                })
+
+    return structure
+
+
+directory = "./src/~"
+
+structure = get_directory_structure(directory)
+
+with open("generated/directory_structure.json", "w", encoding="utf-8") as f:
+    json.dump(structure, f, indent=4)
+
+print("Success!")
--- src/structure.js
+++ src/structure.js
@@ -1,26 +1,39 @@
-class Folder{
+// Helper functions and classes for defining the directory/files
+function pad(str, len){
+    // for(let i = str.length; i < len; i ++){
+    //     str = str + "&nbsp;"
+    // }
+    for(let i = 0; i < len; i ++){
+        str = str + "&nbsp;";
+    }
+    return str;
+}
+
+export class Folder{
     constructor(name){
         this.name = name;
         this.subfolders = [];
         this.files = [];
-        this.path = "/" + name;
+        this.path = name;
+        this.parent = undefined;
     }
 
     addFolder(folder){
         this.subfolders.push(folder);
         folder.path = this.path + "/" + folder.name;
+        folder.parent = this;
     }
 
     addFile(file){
-        this.files.add(file);
+        this.files.push(file);
     }
 
     toString(){
-        all = []
-        for(file in files){
+        var all = [];
+        for(let file of this.files){
             all.push(file);
         }
-        for(subfolder in subfolders){
+        for(let subfolder of this.subfolders){
             all.push(subfolder);
         }
 
@@ -28,18 +41,36 @@ class Folder{
             return a.name.localeCompare(b.name);
         })
 
-        res = ""
-        for(f in all){
-            res += f.name.padStart(10);
+        var res = ""
+        var width = 3;
+        if(this.parent) res += "<span class='blue'>" + pad("../", width) + "<\span>";
+        for(let f of all){
+            if(f instanceof Folder){
+                res += "<span class='blue'>" + pad(f.name + "/", width) + "<\span>";
+            }
+            else{
+                res += "<span class='aqua'>" + pad(f.name, width) + "<\span>";
+            }
         }
-        return res + "<br>";
+        return res;
     }
 }
 
-class File{
-    constructor(name, content){
+export class File{
+    constructor(name, content, is_text){
         this.name = name;
         this.content = content;
+        this.is_text = is_text
+    }
+
+    getContent(){
+        if(!this.is_text){
+            window.open(this.content, '_blank');
+            return undefined;
+        }
+        else{
+            return this.content;
+        }
     }
 }
 
